9313bea806a04eb0b882e6d77d1a14c37f6edf25
larissadantier <larissa_dantier@hotmail.com>
2022-08-20 17:05:28 -0300

    CORRECAO: corrigindo configurações

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react'
-
 function App() {
   return (
-    <div className='App'>
-      <h1>RIRIRIRI</h1>
+    <div className="App">
+      <h1> RIRIRIRI </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
